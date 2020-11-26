550a916a15a202b88d2a1025105e667c8b2c8a65
Edvaldo Valente <edvaldovalente@outlook.com>
2020-11-26 15:57:19 +0100

    remove sqlite3

--- app/services/commiters.rb
+++ app/services/commiters.rb
@@ -10,9 +10,6 @@ class Commiters
         @options = { query: query, header: headers }
     end
 
-    # def user
-    #     self.class.get("/users/edvaldo-valente", @options )
-    # end
     def users
         self.class.get("/search/users?q=followers%3A%3E%3D1000&ref=searchresults&s=followers&type=Users", @options )
     end
